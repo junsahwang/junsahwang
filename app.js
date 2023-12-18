b4ee52825aeeca07dc0bb326a804cf73847d7df2
JunsaHwang <junsaemail@gmail.com>
2023-12-18 00:51:45 -0800

    coolshit

--- app.js
+++ app.js
@@ -9,3 +9,35 @@ function closeMediaSidebar() {
   mediasidebar.style.left = "-100%"; 
 }
 /* SIDENAV *//* SIDENAV *//* SIDENAV *//* SIDENAV *//* SIDENAV *//* SIDENAV *//* SIDENAV */
+
+/* COOLPOPUP *//* COOLPOPUP *//* COOLPOPUP *//* COOLPOPUP *//* COOLPOPUP *//* COOLPOPUP *//* COOLPOPUP */
+function openPopup(id) {
+  const popup = document.getElementById(`popup-${id}`);
+  const overlay = document.getElementById('overlay');
+
+  popup.style.display = 'block';
+  overlay.style.display = 'block';
+}
+
+function closePopup(id) {
+  const popup = document.getElementById(`popup-${id}`);
+  const overlay = document.getElementById('overlay');
+
+  popup.style.display = 'none';
+  overlay.style.display = 'none';
+}
+
+function closeAllPopups() {
+  const popups = document.querySelectorAll('.popup');
+
+  popups.forEach(popup => {
+    popup.style.display = 'none';
+  });
+
+  overlay.style.display = 'none';
+}
+/* COOLPOPUP *//* COOLPOPUP *//* COOLPOPUP *//* COOLPOPUP *//* COOLPOPUP *//* COOLPOPUP */
+
+/* LEGOSWITCH *//* LEGOSWITCH *//* LEGOSWITCH *//* LEGOSWITCH *//* LEGOSWITCH *//* LEGOSWITCH */
+
+/* LEGOSWITCH *//* LEGOSWITCH *//* LEGOSWITCH *//* LEGOSWITCH *//* LEGOSWITCH *//* LEGOSWITCH *//* LEGOSWITCH */
