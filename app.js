fd8e0127b730199aa3749d9cfcf0ca4d76b788c2
JunsaHwang <junsaemail@gmail.com>
2025-01-04 14:35:38 -0800

    audio

--- app.js
+++ app.js
@@ -8,3 +8,29 @@ function closeMediasidebar() {
   const mediaSidebar = document.getElementById('mediasidebar');
   mediaSidebar.style.left = '-100%';  
 }
+
+/* CHA-CHING */
+const hoverLink = document.querySelector('.hover-link');
+const audio = document.getElementById('hover-audio');
+
+hoverLink.addEventListener('mouseenter', () => {
+    audio.play(); 
+});
+
+hoverLink.addEventListener('mouseleave', () => {
+    audio.pause();
+    audio.currentTime = 0;
+});
+
+// Mobile touch
+hoverLink.addEventListener('touchstart', () => {
+  audio.currentTime = 0;  // Start from the beginning
+  audio.play();
+  hoverLink.classList.add('active'); // Optional: Add active class for touch effect
+});
+
+hoverLink.addEventListener('touchend', () => {
+  audio.pause();
+  audio.currentTime = 0;  // Reset to the beginning
+  hoverLink.classList.remove('active'); // Remove active class when touch ends
+});
