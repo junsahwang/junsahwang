5a8f1dd0ddddf6702fc7c7507fd7f91720891746
JunsaHwang <junsaemail@gmail.com>
2025-01-03 17:15:34 -0800

    sidebar

---
+++ app.js
@@ -0,0 +1,10 @@
+
+function openMediasidebar() {
+  const mediaSidebar = document.getElementById('mediasidebar');
+  mediaSidebar.style.left = '0';  
+}
+
+function closeMediasidebar() {
+  const mediaSidebar = document.getElementById('mediasidebar');
+  mediaSidebar.style.left = '-100%';  
+}
